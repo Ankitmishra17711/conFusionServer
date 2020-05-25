45c43dac72c0c2ab1c6d916317bdab49d06e37ab
Ankit Mishra <ankitxvii@gmail.com>
2020-05-25 18:22:17 +0530

    express-session part-2

---
+++ models/user.js
@@ -0,0 +1,22 @@
+// will track user name and password and a flag will be set to indicate  whether the user is  a administrator or normal user
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:  {
+        type: String,
+        required: true
+    },
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', User); 
